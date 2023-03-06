efaf57f28bb7c7dc60f7b1e496c679cc8de91da9
fazalm <105409774+fazalmahmood30@users.noreply.github.com>
2023-03-06 03:13:30 +0000

    my first change

---
+++ route53.tf
@@ -0,0 +1,30 @@
+# get hosted zone details
+# terraform aws data hosted zone
+data "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name
+}
+
+# create a record set in route 53
+# terraform aws route 53 record
+resource "aws_route53_record" "site_domain" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.record_name1
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.test.dns_name
+    zone_id                = aws_lb.test.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "site_domain2" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.record_name2
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.test.dns_name
+    zone_id                = aws_lb.test.zone_id
+    evaluate_target_health = true
+  }
